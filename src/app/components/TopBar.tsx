1227b0abaf957cec8194700e2b69b5add1feb4c8
dj <darrellbrand@gmail.com>
2025-07-27 12:51:24 -0700

    wip

--- src/app/components/TopBar.tsx
+++ src/app/components/TopBar.tsx
@@ -1,5 +1,5 @@
 
-
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Navbar() {
@@ -8,12 +8,22 @@ export default function Navbar() {
       <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex flex-row  justify-between items-center h-16">
           {/* Logo */}
-          <Link href="/" className="md:text-2xl text-sm font-bold text-blue-600">
-            805 Cleaning Crew
+          <Link href="#home" className="md:text-2xl text-sm font-bold flex items-center text-blue-600">
+            <Image
+              src="/bucket.png"
+              alt="805 Cleaning Crew Logo"
+              width={60}
+              height={60}
+              className="object-contain"
+              priority
+            />
           </Link>
 
           {/* Navigation Links */}
-          <ul className=" flex space-x-4 text-blue-600  font-bold text-sm md:text-2xl">
+          <ul className=" flex space-x-4 text-blue-600  font-bold text-lg md:text-2xl">
+            <li>
+              <a href="#home" className="hover:text-blue-900 transition ">home</a>
+            </li>
             <li>
               <a href="#about" className="hover:text-blue-900 transition ">About</a>
             </li>
