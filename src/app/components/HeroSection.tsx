5e686993a089c9302dbda02306b7349899df9c69
dj <darrellbrand@gmail.com>
2025-07-26 21:19:08 -0700

    resize

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 
 export default function HeroSection() {
     return (
-        <section className="relative md:h-[80vh] h-[50vh] w-screen overflow-hidden bg-blue-200 flex items-center justify-center">
+        <section className="relative md:h-[100vh] h-[50vh] w-screen overflow-hidden bg-blue-200 flex items-center justify-center">
             {/* Background Image Container */}
             <div className="absolute inset-0 z-0 flex items-center justify-center pt-10">
                 <div className="relative w-full h-full">
@@ -13,7 +13,7 @@ export default function HeroSection() {
                         src="/hospital.png"
                         alt="Background"
                         fill
-                        className="object-cover object-center opacity-90 mix-blend-overlay"
+                        className="object-fit object-center opacity-90 mix-blend-overlay"
                         priority
                     />
                 </div>
@@ -23,9 +23,9 @@ export default function HeroSection() {
             </div>
 
             {/* Hero Content */}
-            <div className="relative z-10 flex flex-col items-center justify-center text-blue-100 text-center px-6  max-w-5xl mx-auto  pt-15  gap-6 md:gap-15  ">
-                <h1 className="text-2xl md:text-6xl font-extrabold leading-tight">
-                    Spotless Results Every Time
+            <div className="relative z-10 flex flex-col items-center justify-center text-blue-100 text-center px-6  max-w-5xl mx-auto  pt-15  gap-10 md:gap-15  ">
+                <h1 className="text-2xl md:text-7xl font-extrabold leading-tight">
+                    805 Cleaning Crew
                 </h1>
                 <p className="text-md md:text-2xl">
                     Trust 805 Cleaning Crew for reliable, top-quality residential and commercial cleaning across the Central Coast.
