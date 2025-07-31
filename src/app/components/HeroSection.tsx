8b0f07d14e5d21ed93d17edfaac633efcf1911fa
dj <darrellbrand@gmail.com>
2025-07-31 14:56:25 -0700

    add effects

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -6,11 +6,11 @@ import Link from 'next/link';
 export default function HeroSection() {
     return (
         <section className=" min-h-[100vh] w-full flex-col overflow-hidden bg-gradient-to-t from-blue-400 from-0% via-blue-500 via-50% to-blue-600 to-100% flex items-center justify-center  gap-8 px-8 py-2" id="home">
-
             <div className=" marker-font text-blue-100 text-center   mx-auto pt-20    ">
-                <h1 className="text-7xl md:text-9xl font-extrabold leading-tight">
+                <h1 className="text-7xl md:text-9xl font-extrabold leading-tight animate-fade-in-up">
                     805 Cleaning Crew
                 </h1>
+
             </div>
 
 
@@ -32,12 +32,12 @@ export default function HeroSection() {
                         <button>
                             <Link
                                 href="#contact"
-                                className="inline-block bg-blue-100 text-blue-600 font-semibold py-3 px-4 md:py-6 md:px-9 rounded-full hover:bg-blue-300 transition duration-300 text-xl md:text-2xl marker-font "
+                                className=" hover:scale-105 transition duration-300   inline-block bg-blue-100 text-blue-600 font-semibold py-3 px-4 md:py-6 md:px-9 rounded-full hover:bg-blue-300  text-xl md:text-2xl marker-font "
                             >
                                 Get a Free Quote
                             </Link>
                         </button>
-                        <div className='relative w-[150px] md:w-[500px] aspect-[4/3] flex-shrink-0'>
+                        <div className='relative w-[150px] md:w-[500px] aspect-[4/3] flex-shrink-0 animate-float'>
                             <Image
                                 src="/spray.png"
                                 alt="Background"
